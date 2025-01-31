e75888faacd0ca9e354c5fe735758737f9bd3a47
Kolkenn <leonel.ponce96@gmail.com>
2025-01-30 22:42:16 -0500

    CH2-LE3 Complete

--- main.sh
+++ main.sh
@@ -1,1 +1,3 @@
 python3 src/main.py
+
+# This command tells Python to run the main.py file in the src directory.
---
+++ src/test_textnode.py
@@ -0,0 +1,31 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq_text(self):
+        node = TextNode("I'm node one", TextType.BOLD)
+        node2 = TextNode("I'm node two", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_neq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD,"www.google.com")
+        self.assertNotEqual(node, node2)
+
+    def test_neq_text_type(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
