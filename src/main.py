7ffc560112bf787fdcef7f162e8dff2c252deaa3
Kolkenn <leonel.ponce96@gmail.com>
2025-03-11 22:18:14 -0400

    Bones of the main function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from textnode import TextNode, TextType
-
+import os,shutil
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    pass
+
+def wipe_public_dir():
+    # Delete all the contents of the destination directory (public) to ensure that the copy is clean.
+    pass
+
+# Recursive function that copies all the contents from a source directory to a destination directory (in our case, static to public)
+def copy_static_to_public():
+    # Copy all files and subdirectories, nested files, etc.
+    pass
 
 
 main()
