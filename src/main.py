7b2b78c21760e41c46aa8c8b995416a4f1cee082
Kolkenn <leonel.ponce96@gmail.com>
2025-03-12 10:03:46 -0400

    CH5-LE1 Complete

--- src/main.py
+++ src/main.py
@@ -2,16 +2,39 @@ from textnode import TextNode, TextType
 import os,shutil
 
 def main():
-    pass
+    recurse_copy_dir("./static","./public")
 
-def wipe_public_dir():
+def wipe_dir(path):
     # Delete all the contents of the destination directory (public) to ensure that the copy is clean.
-    pass
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            print(f"Removing {path} directory")
+            shutil.rmtree(path)
+    print(f"Creating {path} directory.")
+    os.mkdir(path)
 
 # Recursive function that copies all the contents from a source directory to a destination directory (in our case, static to public)
-def copy_static_to_public():
+def recurse_copy_dir(source,destination):
+    wipe_dir(destination)
     # Copy all files and subdirectories, nested files, etc.
-    pass
+    if os.path.exists(source):
+        if os.path.isdir(source):
+            children = os.listdir(source)
+            print(f"List of items inside {source}: {children}")
+            # Recursion
+            for child in children:
+                full_path = os.path.join(source,child)
+                if os.path.isdir(full_path):
+                    full_des_path = os.path.join(destination,child)
+                    recurse_copy_dir(full_path,full_des_path)
+                    continue
+                print(f"Copying {full_path} to {destination}")
+                shutil.copy(full_path,destination)
+        else:
+            raise NotADirectoryError(f"{source} is not a directory.")
+    else:
+        raise FileNotFoundError(f"{source} does not exist.")
+
 
 
 main()
